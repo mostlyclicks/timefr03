77e4ed7c522396ac7b02053cc9b7d0fe7f240aa0
Carlos Interone <carlos@mostlyclicks.com>
2015-02-07 21:18:52 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timefr03::Application.config.secret_token = 'cf62faf57955499cee9c1e73454df7d1b8607dcf8d0dd86cd0e298ada49da102f463635c9c1d519605517d44e5e44650372f5c3be9df08ef90a21be127b9e054'
