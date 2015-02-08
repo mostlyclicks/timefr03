485d402aa8e0ffa315620e61db12beeb12d1eaa4
Carlos Interone <carlos@mostlyclicks.com>
2015-02-07 21:45:24 -0600

    set initialize on precompile to false

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,10 @@ module Timefr03
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    #http://stackoverflow.com/questions/9722337/refinery-error-when-pushing-to-heroku
+    config.assets.initialize_on_precompile = false
+
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
