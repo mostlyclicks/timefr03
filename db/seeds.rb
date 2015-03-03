7c6e21eda7aab0a2f0cacea3127edb233761395f
Carlos Interone <carlos@mostlyclicks.com>
2015-03-02 21:49:08 -0600

    set to en only for db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -54,6 +56,39 @@ Refinery::Pedals::Engine.load_seed
 # end
 # Added by Refinery CMS Framesets extension
 Refinery::Framesets::Engine.load_seed
+
+@framesets = [
+    {name: 'Skylon Team AKTIV', color: 'Team', description: 'Skylon with AKTIV fork and Directlink seat mast in Team paint scheme.'},
+    {name: 'Skylon White AKTIV', color: 'White', description: 'Skylon with AKTIV fork and Directlink seat mast in white.'},
+    {name: 'Skylon Red AKTIV', color: 'Red', description: 'Skylon with AKTIV fork and Directlink seat mast in red.'},
+    {name: 'Skylon Plasma AKTIV', color: 'Plasma', description: 'Skylon with AKTIV fork and Directlink seat mast in plasma.'},
+
+    {name: 'Skylon Team', color: 'Team', description: 'Skylon in Team paint scheme.'},
+    {name: 'Skylon White', color: 'White', description: 'Skylon in white.'},
+    {name: 'Skylon Red', color: 'Red', description: 'Skylon in red.'},
+    {name: 'Skylon Plasma', color: 'Plasma', description: 'Skylon in plasma.'},
+
+    {name: 'Izon White AKTIV', color: 'White', description: 'Izon with AKTIV fork and Translink seat mast in white.'},
+    {name: 'Izon Red AKTIV', color: 'Red', description: 'Izon with AKTIV fork and Translink seat mast in red.'},
+
+    {name: 'Izon Plasma', color: 'Plasma', description: 'Izon in plasma.'},
+    {name: 'Izon Red', color: 'Red', description: 'Izon in black carbon with red.'},
+
+    {name: 'Fluidity Red AKTIV', color: 'Red', description: 'Fluidity with AKTIV fork and Translink seat mast in red.'},
+    {name: 'Fluidity Black AKTIV', color: 'Black', description: 'Fluidity with AKTIV fork and Translink seat mast in black.'},
+    {name: 'Fluidity Yellow AKTIV', color: 'Yellow', description: 'Fluidity with AKTIV fork and Translink seat mast in yellow.'},
+
+    {name: 'Fluidity DISC Red', color: 'Red', description: 'Fluidity in red with DISC brakes.'},
+    {name: 'Fluidity White', color: 'White', description: 'Fluidity in white.'}
+  ]
+
+  @framesets.each do |fs|
+    Refinery::Framesets::Frameset.create(
+      name: fs[:name],
+      color: fs[:color],
+      description: fs[:description]
+    )
+  end
 
 
  
--- vendor/extensions/framesets/db/seeds.rb
+++ vendor/extensions/framesets/db/seeds.rb
@@ -10,37 +10,6 @@ Refinery::I18n.frontend_locales.each do |lang|
     end
   end
 
-  @framesets = [
-    {name: 'Skylon Team AKTIV', color: 'Team', description: 'Skylon with AKTIV fork and Directlink seat mast in Team paint scheme.', locale: 'EN_en'},
-    {name: 'Skylon White AKTIV', color: 'White', description: 'Skylon with AKTIV fork and Directlink seat mast in white.', locale: 'EN_en'},
-    {name: 'Skylon Red AKTIV', color: 'Red', description: 'Skylon with AKTIV fork and Directlink seat mast in red.', locale: 'EN_en'},
-    {name: 'Skylon Plasma AKTIV', color: 'Plasma', description: 'Skylon with AKTIV fork and Directlink seat mast in plasma.', locale: 'EN_en'},
-
-    {name: 'Skylon Team', color: 'Team', description: 'Skylon in Team paint scheme.', locale: 'EN_en'},
-    {name: 'Skylon White', color: 'White', description: 'Skylon in white.', locale: 'EN_en'},
-    {name: 'Skylon Red', color: 'Red', description: 'Skylon in red.', locale: 'EN_en'},
-    {name: 'Skylon Plasma', color: 'Plasma', description: 'Skylon in plasma.', locale: 'EN_en'},
-
-    {name: 'Izon White AKTIV', color: 'White', description: 'Izon with AKTIV fork and Translink seat mast in white.', locale: 'EN_en'},
-    {name: 'Izon Red AKTIV', color: 'Red', description: 'Izon with AKTIV fork and Translink seat mast in red.', locale: 'EN_en'},
-
-    {name: 'Izon Plasma', color: 'Plasma', description: 'Izon in plasma.'},
-    {name: 'Izon Red', color: 'Red', description: 'Izon in black carbon with red.'},
-
-    {name: 'Fluidity Red AKTIV', color: 'Red', description: 'Fluidity with AKTIV fork and Translink seat mast in red.', locale: 'EN_en'},
-    {name: 'Fluidity Black AKTIV', color: 'Black', description: 'Fluidity with AKTIV fork and Translink seat mast in black.', locale: 'EN_en'},
-    {name: 'Fluidity Yellow AKTIV', color: 'Yellow', description: 'Fluidity with AKTIV fork and Translink seat mast in yellow.', locale: 'EN_en'},
-
-    {name: 'Fluidity DISC Red', color: 'Red', description: 'Fluidity in red with DISC brakes.', locale: 'EN_en'},
-    {name: 'Fluidity White', color: 'White', description: 'Fluidity in white.', locale: 'EN_en'}
-  ]
-
-  @framesets.each do |fs|
-    Refinery::Framesets::Frameset.create(
-      name: fs[:name],
-      color: fs[:color],
-      description: fs[:description]
-    )
-  end
+  
 
 end
